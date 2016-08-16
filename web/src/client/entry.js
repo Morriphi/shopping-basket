d64aeed358e6d60ca34fc0e112c21d9fcbf91bd7
Philip Morris <phillip.morris@mergermarket.com>
2016-08-16 22:56:03 +0100

    remove DevTools override

--- web/src/client/entry.js
+++ web/src/client/entry.js
@@ -1,5 +1,3 @@
-global.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {}; // prevent react DevTools log
-
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Application = require('../components/shopping-basket-controller');
